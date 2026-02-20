609085a00b8dcea1eaceb2ef4f28a66aed95accb
bicutoru <106106717+victorashino@users.noreply.github.com>
2026-02-20 10:11:39 -0300

    recursividade

--- xadrez.c
+++ xadrez.c
@@ -1,42 +1,57 @@
 #include <stdio.h>
 
-int main() {
-
-  int numeroDeCasasParaTorre = 5;
-
-
-  printf("\n##### Torre #####\n");
-  for (int i = 0; i < numeroDeCasasParaTorre; i++) {
+void torre(int num) {
+  
+  if (num > 0) {
     printf("Direita\n");
+    torre(--num);
   }
+}
 
-  int numeroDeCasasParaBispo = 5;
+void bispo(int num) {
 
-  printf("\n##### Bispo #####\n");
-  while (numeroDeCasasParaBispo > 0) {
-    
+  for (int i = 0; i < num; i++) {
     printf("Cima\n");
     printf("Direita\n");
-    --numeroDeCasasParaBispo;
   }
+}
 
-  int numeroDeCasasRainha = 8;
-
-  printf("\n##### Rainha #####\n");
-  do {
+void rainha(int num) {
+  
+  if (num > 0) {
     printf("Esquerda\n");
-    --numeroDeCasasRainha;
-  } while (numeroDeCasasRainha > 0);
+    rainha(--num);
+  }
+}
 
-  int numeroDeCasasCavalo = 1;
+void cavalo(int num) {
 
-  printf("\n##### Cavalo #####\n");
-  while (numeroDeCasasCavalo--) {
-    for (int i = 0; i < 2; i++) {
+  if (num > 0) {
+    for (int i = 0; i < 3; i++) {
       printf("Cima\n");
     }
     printf("Direita\n");
+    cavalo(--num);
   }
+}
+
+int main() {
+
+  int numeroDeCasasParaTorre = 5;
+  printf("##### Torre #####\n");
+  torre(numeroDeCasasParaTorre);
+
+  int numeroDeCasasParaBispo = 5;
+  printf("\n##### Bispo #####\n");
+  bispo(numeroDeCasasParaBispo);
+
+  int numeroDeCasasRainha = 8;
+  printf("\n##### Rainha #####\n");
+  rainha(numeroDeCasasRainha);
+
+  int numeroDeCasasCavalo = 1;
+  printf("\n##### Cavalo #####\n");
+  cavalo(numeroDeCasasCavalo);
 
   return 0;
 }
